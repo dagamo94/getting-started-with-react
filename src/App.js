c2f321ecea991b9cab15ebf354de2682cd1fbb89
dan <da.ga.mo.94@gmail.com>
2022-03-14 08:32:06 -0600

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function WelcomeBack({ name = "valued customer", adjective = "nice" }) {
+  return (
+    <p>
+      Welcome back, {adjective} {name}!
+    </p>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <WelcomeBack name="Joe" adjective="funny" />
+      <WelcomeBack name="Anna" adjective="clever" />
+      <WelcomeBack />
+    </>
   );
 }
 
