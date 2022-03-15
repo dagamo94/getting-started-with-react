04d9315732ed0ebb461d16f54d1634c2bc59bcdb
dan <da.ga.mo.94@gmail.com>
2022-03-15 13:12:09 -0600

    update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Header from "./Header";
 import Greeting from "./Greeting";
 import Notifications from "./Notifications";
 import ShoppingList from "./ShoppingList";
+import Quote from "./Quote";
 
 const todos = [
   { completed: false, description: "Finish the Lists & Tables checkpoint" },
   { completed: false, description: "Clean my desk" },
   { completed: false, description: "Make lunch" },
 ];
+
+const quote = {
+  text:
+    "I am great believer in luck, and I find the harder I work, the more I have of it.",
+  author: "Thomas Jefferson",
+};
 
 function App() {
   const notifications = ["You received a package", "The weather is sunny"];
@@ -23,6 +30,7 @@ function App() {
       <WelcomeBack name="Anna" adjective="clever" />
       <WelcomeBack />
       <ShoppingList items={todos} />
+      <Quote quote={quote} />
       <Footer />
     </>
   );
--- src/WelcomeBack.js
+++ src/WelcomeBack.js
@@ -1,7 +1,18 @@
 function WelcomeBack({ name = "valued customer", adjective = "nice" }) {
+  const pStyle = {
+    color: "black",
+    fontFamily: "Zen Dots, cursive",
+  };
+  const userName = {
+    padding: "2px 4px",
+    marginRight: "2px",
+    backgroundColor: "#000",
+    color: "#fff",
+    borderRadius: "4px",
+  };
     return (
-      <p>
-        Welcome back, {adjective} {name}!
+      <p style={pStyle}>
+        Welcome back, <span style={userName}>{adjective} {name}</span>!
       </p>
     );
   }
